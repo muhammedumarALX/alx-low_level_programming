fd6e7c269963dd4e324fdf1258e79bc428ef782f
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-08 17:39:36 -0700

    Task *

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char Mystr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-	putchar(Mystr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59)
 
 	return (1);
 }
