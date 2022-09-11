9fe22770458be3101934f41e0027811ab24fcdbd
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-11 16:48:19 -0700

    Task &

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	/*Declare Variable*/
+	char i;
+	/*program to run*/
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
+	/*output*/
+	putchar('\n');
+	return (0);
+}
