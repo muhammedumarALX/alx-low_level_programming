ec16f6d366d0fcc9a6b8ea1a4593a859fca5256e
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-11 17:10:29 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*declare variables*/
+	char i;
+	int j;
+	
+	/*inserting numeric values*/
+	for (j = 48; j < 58; j++)
+		putchar(j);
+	/*inserting alphabetic values*/
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
