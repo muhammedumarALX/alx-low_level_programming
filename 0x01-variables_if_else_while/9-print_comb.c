e9ee1b786fe5fd795a7e802c49d59dcc3b573146
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-12 20:00:57 -0700

    Task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,19 +5,21 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
-int i;
+	/*delcare variable*/
+	int i;
 
-for (i = 48; i < 58; i++)
-{
+	for (i =48; i < 58; i++)
+	{
 	putchar(i);
 	if (i != 57)
-	{
-	putchar(',');
-	putchar(' ');
+		{
+		putchar(',');
+		putchar(' ');
+		}
 	}
-}
-putchar('\n');
-return (0);
+
+	putchar('\n');
+	return (0);
 }
