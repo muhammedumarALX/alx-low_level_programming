da30685dc221f34cbb94e55169953302ecadfed7
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-14 00:32:33 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of an int
+ * @c: is the int that uses the argument function
+ * Return: Always 0 
+ */
+int _abs(int c);
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
