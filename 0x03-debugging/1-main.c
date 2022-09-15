b1bb6029cd38136f2a67452f4287bd359eb4b1e1
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-15 02:28:32 -0700

    Task 1

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - cause an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinte loop incoming :( \n");
+
+	i = 0;
+/**
+*	while (i < 10)
+*	{
+*		putchar(i);
+*	}
+*/
+	printf("infinite loop avoided! \\0/\n");
+
+	return (0);
+}
