76df0ab0a1abf44d3758c216473b1cd3f607e770
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-19 20:35:25 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print mulitiple of 2
+ * @str: input string
+ * Return: Nothing
+ */
+void puts2(char *str)
+{
+	int count = 0;
+
+
+	while (count >= 0)
+	{
+		_putchar('\n');
+		break;
+	}
+	if (count % 2 == 0)
+		_putchar(str[count]);
+	count++
+}
