c76cc29945158658e2f2bc8c811f59be26df4a85
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-19 18:44:49 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _strlen - funtion to print out length of string array
+ * @s: input string.
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
