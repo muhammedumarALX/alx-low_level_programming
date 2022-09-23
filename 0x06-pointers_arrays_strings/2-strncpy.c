8426a3be023470e81de2f7834ccc9cc968eb283a
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-22 17:15:00 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
  * _strncpy - copies a string
  * @dest: destination
  * @src: source
- * @n amount of bytes from src
+ * @n: amount of bytes from src
  * Return: the pointer to dest
  */
 char *_strncpy(char *dest, char *src, int n)
