b045ff092122223f847cbd5100bbcba88b2a2ab4
muhammedumarALX <muhammedumaralx@gmail.com>
2022-09-26 16:10:54 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,8 +10,9 @@ char *_strchr(char *s, char c)
 {
 	unsigned int i = 0;
 
-	for (; *(s + i) == c)
-		return (s + i);
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
 	if (*(s + i) == c)
 		return (s + i);
 	return ('\0');
