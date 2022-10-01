4af9e0148fc8837ef2293b32b96fbf632485407e
muhammedumarALX <muhammedumaralx@gmail.com>
2022-10-01 15:43:01 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints it name followed by new line
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Nothing
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
