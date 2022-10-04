1b60e40b5ebdb62390165832b117093b8ccf9f21
muhammedumarALX <muhammedumaralx@gmail.com>
2022-10-03 20:43:03 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - adds two numbers
@@ -9,11 +10,20 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
 	{
-		printf("Error\n");
-		return (1);
+		for  (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+	printf("%d\n", add);
 	return (0);
 }
