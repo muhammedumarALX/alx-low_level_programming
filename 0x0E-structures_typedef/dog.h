5d01ce95745b499e10b974353f9a6b965930075f
muhammedumarALX <muhammedumaralx@gmail.com>
2022-10-10 16:28:41 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H-
+#define _DOG_H_
+
+/**
+ * struct dog - structure that stores some information of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and them owners name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
