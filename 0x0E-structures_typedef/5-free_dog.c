5203c9464738230c633b7b11d329188e32c1b711
muhammedumarALX <muhammedumaralx@gmail.com>
2022-10-10 18:10:08 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlb.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: struct dog
+ *
+ * Return: nil
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
